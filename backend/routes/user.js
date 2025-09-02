f149c6b16e41ec349239de78ae7649d3f0b56e49
sungbbang <sinsa5133@gmail.com>
2025-09-02 11:35:16 +0900

    유저 로그인 엔드포인트 및 api 구현

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/signup', async (req, res) => {
   try {
@@ -39,6 +40,81 @@ router.post('/signup', async (req, res) => {
       .json({ status: 'success', message: '회원가입이 완료되었습니다.' });
   } catch (err) {
     console.error(err);
+    res.status(500).json({ status: 'error', message: 'Server error' });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+      return res.status(404).json({ message: '사용자를 찾을 수 없습니다.' });
+    }
+
+    if (!user.isActive) {
+      return res.status(401).json({ message: '비활성화된 계정입니다.' });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      user.loginAttempts += 1;
+
+      if (user.loginAttempts >= 5) {
+        user.isActive = false;
+        await user.save();
+        return res.status(401).json({
+          status: 'error',
+          message: '비밀번호를 5회 이상 틀려 계정이 비활성화되었습니다.',
+        });
+      }
+
+      await user.save();
+      return res.status(401).json({
+        status: 'error',
+        message: `비밀번호가 틀렸습니다. 남은 횟수는 ${
+          5 - user.loginAttempts
+        }회입니다.`,
+        remainingAttemps: 5 - user.loginAttempts,
+      });
+    }
+
+    user.loginAttempts = 0;
+    user.isActive = true;
+
+    await user.save();
+
+    const payload = {
+      _id: user._id,
+      email: user.email,
+      nickname: user.nickname,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+      expiresIn: '24h',
+    });
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    const userResponse = {
+      _id: user._id,
+      email: user.email,
+      nickname: user.nickname,
+    };
+
+    return res.json({
+      status: 'success',
+      user: userResponse,
+    });
+  } catch (error) {
+    console.error(error);
     res.status(500).json({ status: 'error', message: 'Server error' });
   }
 });
