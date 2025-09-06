0e63154f257d121d500e68586f761efb0b79fe43
sungbbang <sinsa5133@gmail.com>
2025-09-06 10:33:38 +0900

    댓글 삭제 api 구현

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -90,4 +90,31 @@ router.patch('/:commentId', authenticateToken, async (req, res) => {
   }
 });
 
+router.delete('/:id', authenticateToken, async (req, res) => {
+  try {
+    const commentId = req.params.id;
+    const userId = req.user._id;
+
+    const comment = await Comment.findById(commentId);
+
+    if (comment.authorId.toString() !== userId.toString()) {
+      return res.status(403).json({
+        status: 'error',
+        message: '작성자가 아니므로 권한이 없습니다.',
+      });
+    }
+
+    await comment.deleteOne();
+
+    await Post.findByIdAndUpdate(comment.postId, {
+      $inc: { commentsCount: -1 },
+    });
+
+    res.json({ status: 'success', message: '댓글이 삭제되었습니다.' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ status: 'error', message: 'Server error' });
+  }
+});
+
 module.exports = router;
