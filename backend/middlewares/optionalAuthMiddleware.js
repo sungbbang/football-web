f51c962b4c40be12e006e352f5e7634109bf5982
sungbbang <sinsa5133@gmail.com>
2025-09-07 19:37:54 +0900

    optionalAuthenticateToken 미들웨어 구현

---
+++ backend/middlewares/optionalAuthMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const optionalAuthenticateToken = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    req.user = null;
+    return next();
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      req.user = null;
+    } else {
+      req.user = user;
+    }
+    next();
+  });
+};
+
+module.exports = { optionalAuthenticateToken };
