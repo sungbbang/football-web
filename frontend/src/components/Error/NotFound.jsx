93c4b6a151506cb60c6499e722d3650204d93e41
sungbbang <sinsa5133@gmail.com>
2025-09-08 20:21:28 +0900

    에러 시 리다이렉트 로직 수정

--- frontend/src/components/Error/NotFound.jsx
+++ frontend/src/components/Error/NotFound.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 function NotFound() {
-  const navigate = useNavigate();
   return (
     <div className='flex min-h-dvh flex-col items-center justify-center space-y-10'>
-      <h2 className='text-xl lg:text-2xl'>404 | 페이지를 찾을 수 없습니다.</h2>
+      <h2 className='text-center text-xl lg:text-2xl'>
+        404 | 페이지를 찾을 수 없습니다.
+      </h2>
       <button
         className='cursor-pointer rounded-lg border p-3 hover:bg-gray-400 hover:text-white'
-        onClick={() => navigate('/', { replace: true })}
+        onClick={() => window.location.replace('/')}
       >
         메인으로 돌아가기
       </button>
--- frontend/src/routes/ErrorBoundary.jsx
+++ frontend/src/routes/ErrorBoundary.jsx
@@ -1,19 +1,18 @@
-import { isRouteErrorResponse, useNavigate, useRouteError } from 'react-router';
+import { isRouteErrorResponse, useRouteError } from 'react-router';
 
 export function ErrorBoundary() {
   const error = useRouteError();
-  const navigate = useNavigate();
   console.log('error: ', error);
 
   return (
     <div className='flex min-h-dvh flex-col items-center justify-center space-y-10'>
-      <h2 className='text-xl lg:text-2xl'>
+      <h2 className='text-center text-xl lg:text-2xl'>
         {isRouteErrorResponse(error) && `${error.status} ${error.statusText}`}
         {error instanceof Error && `${error.message}`}
       </h2>
       <button
         className='cursor-pointer rounded-lg border p-3 hover:bg-gray-400 hover:text-white'
-        onClick={() => navigate('/', { replace: true })}
+        onClick={() => window.location.replace('/')}
       >
         메인으로 돌아가기
       </button>
